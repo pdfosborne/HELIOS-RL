ba19b8d6bfdd878282f7ecd2db9a5350335e3c7e
pdfosborne <pdfosborne@gmail.com>
2023-05-17 14:41:28 +0100

    v0.4.0 - updated instr episode learning

--- helios/__init__.py
+++
@@ -1,4 +0,0 @@
-from .experiments.standard import Experiment
-from .experiments.supervised_instruction_following import SupervisedExperiment
-from .experiments.unsupervised_instruction_following import UnsupervisedSearch
-from .experiments.helios import HELIOS
